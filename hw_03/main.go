2f24d165b613beeef8b00072a596ccdb28c55574
Vladyslav Babenko <96999713+vlad-bb@users.noreply.github.com>
2025-04-03 09:13:01 +0300

    add files

---
+++ hw_03/main.go
@@ -0,0 +1,62 @@
+package documentstore
+
+import "fmt"
+
+type DocumentFieldType string
+
+const (
+	DocumentFieldTypeString DocumentFieldType = "string"
+	DocumentFieldTypeNumber DocumentFieldType = "number"
+	DocumentFieldTypeBool   DocumentFieldType = "bool"
+	DocumentFieldTypeArray  DocumentFieldType = "array"
+	DocumentFieldTypeObject DocumentFieldType = "object"
+)
+
+type DocumentField struct {
+	Type  DocumentFieldType
+	Value interface{}
+}
+
+type Document struct {
+	Fields map[string]DocumentField
+}
+
+var documents = map[string]Document{}
+
+func Put(doc Document) {
+	// 1. Перевірити що документ містить в мапі поле `key` типу `string`
+	// 2. Додати Document до локальної мапи з документами
+	field, err := doc.Fields["key"]
+	if err != true {
+		fmt.Printf("Not any keys")
+		return
+	}
+
+	if field.Type != DocumentFieldTypeString {
+		fmt.Printf("Invalid  key %v, should be 'string'\n", field.Type)
+		return
+	}
+	documents[field.Value.(string)] = doc
+}
+
+//	func Get(key string) (*Document, bool) {
+//		// Потрібно повернути документ по ключу
+//		// Якщо документ знайдено, повертаємо `true` та поінтер на документ
+//		// Інакше повертаємо `false` та `nil`
+//		// TODO: Implement
+//	}
+//
+//	func Delete(key string) bool {
+//		// Видаляємо документа по ключу.
+//		// Повертаємо `true` якщо ми знайшли і видалили документі
+//		// Повертаємо `false` якщо документ не знайдено
+//		// TODO: Implement
+//	}
+//
+//	func List() []Document {
+//		// Повертаємо список усіх документів
+//		// TODO: Implement
+//	}
+func main() {
+	fmt.Println(documents)
+}
