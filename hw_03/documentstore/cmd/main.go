ba2a0f42fe130e7e83036f7c9a6ad35bef89a57a
Vladyslav Babenko <96999713+vlad-bb@users.noreply.github.com>
2025-04-05 21:16:52 +0300

    Revert "finish hw 03"
    This reverts commit 77535bca9216701db0dcd2cff8f860158db789c0.

--- hw_03/documentstore/cmd/main.go
+++ hw_03/documentstore/cmd/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"hw_03/documentstore"
 )
 
@@ -12,51 +11,8 @@ var firstDoc = documentstore.Document{
 	},
 }
 
-var thirdDoc = documentstore.Document{
-	Fields: map[string]documentstore.DocumentField{
-		"key":    {Type: documentstore.DocumentFieldTypeString, Value: "third"},
-		"amount": {Type: documentstore.DocumentFieldTypeNumber, Value: 100},
-	},
-}
-
-var fourthDoc = documentstore.Document{
-	Fields: map[string]documentstore.DocumentField{
-		"key":    {Type: documentstore.DocumentFieldTypeString, Value: "fourth"},
-		"amount": {Type: documentstore.DocumentFieldTypeNumber, Value: 100},
-	},
-}
-
 func main() {
-	documentstore.Put(firstDoc)  // add document
-	documentstore.Put(thirdDoc)  // add document
-	documentstore.Put(fourthDoc) // add document
+	documentstore.Put(firstDoc) // Додаємо документ
 
-	doc, ok := documentstore.Get("first") // success case
-	if ok {
-		fmt.Printf("Document: %v\n", doc)
-	} else {
-		fmt.Println("Document not found")
-	}
-	doc, ok = documentstore.Get("second") // fail case
-	if ok {
-		fmt.Printf("Document: %v\n", doc)
-	} else {
-		fmt.Println("Document not found")
-	}
-
-	status := documentstore.Delete("first") // success case
-	if status {
-		fmt.Println("Document deleted")
-	} else {
-		fmt.Println("Document not found")
-	}
-	status = documentstore.Delete("second") // fail case
-	if status {
-		fmt.Println("Document deleted")
-	} else {
-		fmt.Println("Document not found")
-	}
-
-	docs := documentstore.List()
-	fmt.Println(docs)
+	//fmt.Println(documentstore.documents["first"]) // так не можно добратися до змінної бо вона не доступна бо з малої літери
 }
